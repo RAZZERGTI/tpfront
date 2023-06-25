ff4dffbad0c04db96f9f115bfaf5ef126c6b8eaa
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2023-06-25 21:28:03 +0300

    Update hashUtils.ts

--- src/hash/hashUtils.ts
+++ src/hash/hashUtils.ts
@@ -1,11 +1,7 @@
-const crypto = require('crypto');
+import crypto from 'crypto';
 
-function sha512Hash(input) {
-    const hash = crypto.createHash('sha512');
-    hash.update(input);
-    return hash.digest('hex');
+export function sha512Hash(input: string): string {
+  const hash = crypto.createHash('sha512');
+  hash.update(input);
+  return hash.digest('hex');
 }
-
-module.exports = {
-    sha512Hash
-};
