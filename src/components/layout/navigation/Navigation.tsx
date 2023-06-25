4390c28cbc7e5228319d5264ee0725f52e59bbae
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2023-06-25 22:33:00 +0300

    Update Navigation.tsx

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -7,30 +7,28 @@ import ModalHelp from '@/components/ui/ModalHelp/ModalHelp';
 import { Tooltip } from 'react-tippy';
 
 const Navigation: React.FC = () => {
-  const { pathname } = useRouter();
-  const [active, setActive] = useState(false);
+    const { pathname } = useRouter();
+    const [active, setActive] = useState(false);
 
-  return (
-    <div className={styles.navigation}>
-      {active && <ModalHelp active={active} setActive={setActive} />}
-      <div>
-        <Links linkJPG={pathname === '/dashboard' ? '/redImages/albumRed.svg' : '/album.svg'} name="Альбомы" href="/dashboard" />
-        <Links linkJPG={pathname === '/dashboard/feed' ? '/redImages/feelRed.svg' : '/feel.svg'} name="Лента" href="/dashboard/feed" />
-        <Links linkJPG={pathname === '/dashboard/likes' ? '/redImages/likeRed.svg' : '/like.svg'} name="Лайки" href="/dashboard/likes" />
-      </div>
-      <div>
-        {pathname === '/dashboard' && (
-         <Tooltip title={'Справка'} {...(props as any)}>
-  <button className={styles.help} onClick={() => setActive(true)}>
-    <Image src="/help.svg" height={30} width={30} alt={'Help'} />
-  </button>
-</Tooltip>
-
-
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.navigation}>
+            {active && <ModalHelp active={active} setActive={setActive} />}
+            <div>
+                <Links linkJPG={pathname === '/dashboard' ? '/redImages/albumRed.svg' : '/album.svg'} name="Альбомы" href="/dashboard" />
+                <Links linkJPG={pathname === '/dashboard/feed' ? '/redImages/feelRed.svg' : '/feel.svg'} name="Лента" href="/dashboard/feed" />
+                <Links linkJPG={pathname === '/dashboard/likes' ? '/redImages/likeRed.svg' : '/like.svg'} name="Лайки" href="/dashboard/likes" />
+            </div>
+            <div>
+                {pathname === '/dashboard' && (
+                    <Tooltip title={'Справка'}>
+                        <button className={styles.help} onClick={() => setActive(true)}>
+                            <Image src="/help.svg" height={30} width={30} alt={'Help'} />
+                        </button>
+                    </Tooltip>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Navigation;
