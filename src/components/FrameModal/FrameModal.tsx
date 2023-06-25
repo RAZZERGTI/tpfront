42a5a54b1b315d27d12071e1c995f33d54a700ed
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2023-06-25 20:48:40 +0300

    Update FrameModal.tsx

--- src/components/FrameModal/FrameModal.tsx
+++ src/components/FrameModal/FrameModal.tsx
@@ -1,24 +1,25 @@
-import React, {FC} from 'react';
-import Image from "next/image";
-import Modal from "@/components/ui/ModalFrameWrapper/Modal";
-import styles from './FrameModal.module.scss'
+import React, { FC, ReactNode, SetStateAction, Dispatch } from 'react';
+import Image from 'next/image';
+import Modal from '@/components/ui/ModalFrameWrapper/Modal';
+import styles from './FrameModal.module.scss';
 
 interface IProps {
-    active: boolean
-    setModal: (value: boolean) => void
-    setFrame: (value: number) => void
+  active: boolean;
+  setModal: Dispatch<SetStateAction<boolean>>;
+  setFrame: (value: number) => void;
 }
 
-const FrameModal:FC<IProps> = ({active, setModal, setFrame}) => {
-    const handleChangeFrame = (number: number) => {
-        setModal(false)
-        setFrame(number)
-    }
-    return (
-        <Modal active={active} setActive={(value: boolean) => setModal(value)}>
-              <div className={styles.frame_wrapper}>
-                <h4>Выберите рамку</h4>
-                <div className={styles.frame_images}>
+const FrameModal: FC<IProps> = ({ active, setModal, setFrame }) => {
+  const handleChangeFrame = (number: number) => {
+    setModal(false);
+    setFrame(number);
+  };
+
+  return (
+    <Modal active={active} setActive={setModal}>
+      <div className={styles.frame_wrapper}>
+        <h4>Выберите рамку</h4>
+        <div className={styles.frame_images}>
                     <Image src={`/frame/frame1.apng`}
                            alt={'previous'}
                            width={120}
