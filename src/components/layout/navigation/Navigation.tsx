5310961dcb18cb7f7b08ea3e6ac7f31f0182f7d4
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2023-06-26 22:54:52 +0300

    Update Navigation.tsx

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './Navigation.module.scss';
 import Links from '@/components/layout/header/Links';
@@ -9,6 +9,25 @@ import { Tooltip } from 'react-tippy';
 const Navigation: React.FC = () => {
     const { pathname } = useRouter();
     const [active, setActive] = useState(false);
+    const [shouldHideNavigation, setShouldHideNavigation] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // Проверяем наличие скролла по высоте страницы
+            const hasVerticalScroll = window.innerHeight < document.body.scrollHeight;
+
+            // Если есть скролл, скрываем навигацию
+            setShouldHideNavigation(hasVerticalScroll);
+        };
+
+        // При монтировании компонента добавляем слушатель события прокрутки
+        window.addEventListener('scroll', handleScroll);
+
+        // При размонтировании компонента удаляем слушатель события прокрутки
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div className={styles.navigation}>
@@ -19,7 +38,7 @@ const Navigation: React.FC = () => {
                 <Links linkJPG={pathname === '/dashboard/likes' ? '/redImages/likeRed.svg' : '/like.svg'} name="Лайки" href="/dashboard/likes" />
             </div>
             <div>
-                {pathname === '/dashboard' && (
+                {shouldHideNavigation && pathname === '/dashboard' && (
                     <Tooltip title="Справка">
                         <button className={styles.help} onClick={() => setActive(true)}>
                             <Image src="/help.svg" height={30} width={30} alt="Help" />
